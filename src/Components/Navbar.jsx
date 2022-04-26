935fca61a0ed228149d6084c251cd2764901cc44
Abdelghafour <ghafourragg@gmail.com>
2022-04-26 03:51:00 +0100

    added an icon edited the html file

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../Assets/Pictures/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src={logo} alt="Medical logo" />
+    </nav>
+  );
+};
+
+export default Navbar;
