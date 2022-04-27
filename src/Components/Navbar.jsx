173dd6dec05c0a4c5091535e3336bf58145be768
Abdelghafour <ghafourragg@gmail.com>
2022-04-27 07:02:22 +0100

    added basic scss files

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "../Assets/Pictures/logo.png";
+// import { Button } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="container">
       <img className="logo" src={logo} alt="Medical logo" />
       <h1>This is the first header</h1>
     </nav>
