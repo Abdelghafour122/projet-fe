a1cae8a72cdcbfb604a7433641a09df3dc69e8ca
Abdelghafour <ghafourragg@gmail.com>
2022-04-28 03:14:07 +0100

    added some colors, variables & theme files

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,6 +7,8 @@ const Navbar = () => {
     <nav className="container">
       <img className="logo" src={logo} alt="Medical logo" />
       <h1>This is the first header</h1>
+      <h2>This is the first header</h2>
+      <p>This is the first header</p>
     </nav>
   );
 };
